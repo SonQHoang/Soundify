24f3e6e3814598ff25c74a816f7b83349c9c3b7e
Son Hoang <sonquhoang@gmail.com>
2023-09-06 16:10:51 -0700

    outline general structure of the frontpage

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,25 +4,44 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<div className="nav-bar-container">
-			<div className='nav-bar-left'>
-				<NavLink exact to="/">
-					Home
-				</NavLink>
-			</div>
-			<div className='nav-bar-right'>
-
-			{isLoaded && (
-				<div>
-					<ProfileButton user={sessionUser} />
+		<div className="main-page-container">
+			<div className="side-bar-container">
+				<div className='home-container'>
+					<NavLink exact to="/">
+						Home
+					</NavLink>
 				</div>
-			)}
-		</div>
+				<div className="playlists-container">
+					<h1>Playlists</h1>
+				</div>
+				<div className="library-container">
+					<h1>Your Library</h1>
+				</div>
 			</div>
+			<div className="main-content-container">
+				<div className='nav-bar'>
+					<div className="upcoming-features">
+						<h2>Upcoming Features!</h2>
+					</div>
+					{isLoaded && (
+						<div>
+							<ProfileButton user={sessionUser} />
+						</div>
+					)}
+				</div>
+				<div className="soundify-playlists">
+					<h1>Soundify Playlists</h1>
+				</div>
+				<div className="focus">
+					<h1>Focus</h1>
+				</div>
+			</div>
+
+		</div>
 	);
 }
 
