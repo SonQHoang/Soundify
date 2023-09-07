6154e0e49aa92859345bd958cbb607c2fd843674
Son Hoang <sonquhoang@gmail.com>
2023-09-07 09:22:18 -0700

    Added styling for video player

--- react-app/src/components/AudioBar/audiobar.js
+++ react-app/src/components/AudioBar/audiobar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AudioPlayer from 'react-h5-audio-player';
-// import './custom-audio-player.css';
+import 'react-h5-audio-player/lib/styles.css';
 
 const Player = () => {
     return (
