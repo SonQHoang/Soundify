ca79a9316eb510ecc19840990bd621b5fffd3dbc
Son Hoang <sonquhoang@gmail.com>
2023-11-08 23:13:14 -0800

    Code annotation

--- react-app/src/components/AudioBar/audiobar.js
+++ react-app/src/components/AudioBar/audiobar.js
@@ -6,7 +6,7 @@ import './audiobar.css';
 import { SongContext } from '../../context/SongContext';
 
 const Player = () => {
-    const playerRef = useRef(); // useRef persistently stores a mutable value that doesn't cause a re-render. Think of a "box" holding a mutable (Can be modified) value
+    const playerRef = useRef(); // useRef persistently stores a mutable value that DOESN'T cause a re-render. Think of a "box" holding a mutable (Can be modified) value
                                 // All ref objects have a property called .current. If you don't provide useRef with an arg, .current is undefined UNTIL the component its attached to mounts.
     const { isAuthenticated } = useContext(AuthContext); // Using context to gain access to isAuth from AuthContext to determine if audio player should be rendered
 
@@ -35,17 +35,19 @@ const Player = () => {
 
     useEffect(() => {
         const audioElement = playerRef.current?.audio.current;
+        // playerRef.current will be the instance of the AudioPlayer once the component mounts
+        // audio.current holds the value of the actual audio element
         if (audioElement) {
-            if (isPlaying) {
-                audioElement.play();
+            if (isPlaying) { 
+                audioElement.play(); // if isPlaying is truthy, audio will play
             } else {
-                audioElement.pause();
+                audioElement.pause(); // else audio will pause
             }
         }
     }, [isPlaying]);
 
-    return (
-        isAuthenticated && currentSong ? (
+    return ( // Rendering...
+        isAuthenticated && currentSong ? ( // if user is logged in and currentSong is truthy
             <div className="audio-bar-container">
                 <div className="audio-bar-song-info">
                     <div className="audio-bar-album-cover">
@@ -57,17 +59,19 @@ const Player = () => {
                     </div>
                 </div>
                 <AudioPlayer
-                    ref={playerRef}
+                    ref={playerRef} // This allows us to access the instance of the AudioPlayer component throughout the component by setting playerRef's .current property to the mounted instance of AudioPlayer
+                                    // As a result, once AudioPlayer mounts, we can access any properties or methods that are exposed by the instance
+                                    // We're able to continually hold ont this value across renders without causing a re-render
                     autoPlayAfterSrcChange={false}
-                    src={currentSong}
-                    onPlay={play} 
-                    onPause={pause} 
+                    src={currentSong} // CurrentSong that is playing
+                    onPlay={play} // Controls play (From Song context)
+                    onPause={pause} // Controls pause (From Song Context)
                     listenInterval={100}
-                    autoPlay={isPlaying}
-                    showSkipControls={true}
-                    showJumpControls={true}
-                    onClickNext={() => handleNextTrack(songs)}
-                    onClickPrevious={() => handlePreviousTrack(songs)}
+                    autoPlay={isPlaying} // Audio will start playing automatically if isPlaying === True
+                    showSkipControls={true} //Enables skip controls to be visible
+                    showJumpControls={true} // Enables jump controls to be visible
+                    onClickNext={() => handleNextTrack(songs)} // Next button functionality
+                    onClickPrevious={() => handlePreviousTrack(songs)} // Previosu button funtionality
                 />
             </div>
         ) : null
